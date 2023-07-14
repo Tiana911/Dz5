836666611244aa987be001c1bbac671431313612
Burshtyn Tanya <kruamge@gmail.com>
2023-07-14 12:44:25 +0300

    dz

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int Prompt(string);
+Console.Write("Введите число: ");
+srting value = Console.readLine();
+int result = Convert.ToInt32(value);
+return result;
