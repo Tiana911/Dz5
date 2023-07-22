2e7d26a1a7eae7235fcc1b5191e70d79f29b0a6b
Burshtyn Tanya <kruamge@gmail.com>
2023-07-22 15:43:05 +0300

    dz

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -3,19 +3,19 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.Write("Введите число (через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
+// Console.Write("Введите число (через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
  
-for (int i = 0; i < arr.Length; i++)
-{
-    if (arr[i] > 0)
-    {
-        count++;
-    }
-}
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0)
+//     {
+//         count++;
+//     }
+// }
  
-Console.WriteLine($"Кол-во элементов > 0: {count}");
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
